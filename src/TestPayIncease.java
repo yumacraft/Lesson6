441f17fa2f1d2cc80fe0f058da73a5f8bbe2fa1a
Dmitriy <lifeviev@gmai.com>
2017-07-28 15:57:43 +0300

    Строка изменения

--- src/TestPayIncease.java
+++ src/TestPayIncease.java
@@ -6,12 +6,16 @@ public class TestPayIncease {
 	/**
 	 * @param args
 	 */
+	
+
 	public static void main(String[] args) {
 
         Person workers[] = new Person[3];
 		workers[0] = new Employee("John");
 		workers[1] = new Contractor("Mary");
 		workers[2] = new Employee("Steve");
+		
+		System.out.println("Изменения");
 		
 		     Employee currentEmployee;
 		     Contractor currentContractor; 
